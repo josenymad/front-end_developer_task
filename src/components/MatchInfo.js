c8a5187479684c900f8591e545387f6020e665bc
Jose Wills <jose.wills3@gmail.com>
2023-09-01 21:25:09 +0100

    added images

--- src/components/MatchInfo.js
+++ src/components/MatchInfo.js
@@ -1,4 +1,6 @@
 import React from "react";
+import arsenalBadge from "../images/arsenal.png";
+import fulhamBadge from "../images/fulham.png";
 
 const MatchInfo = ({ teams, scores }) => {
   const { homeTeam, awayTeam } = teams;
@@ -6,10 +8,20 @@ const MatchInfo = ({ teams, scores }) => {
   return (
     <div className="match-info">
       <div className="match-info__home-team">
+        <img
+          src={arsenalBadge}
+          alt="Home Team Badge"
+          className="match-info__home-team__badge"
+        />
         <h2 className="match-info__home-team__name">{homeTeam}</h2>
         <h3 className="match-info__home-team__score">{homeScore}</h3>
       </div>
       <div className="match-info__away-team">
+        <img
+          src={fulhamBadge}
+          alt="Away Team Badge"
+          className="match-info__away-team__badge"
+        />
         <h2 className="match-info__away-team__name">{awayTeam}</h2>
         <h3 className="match-info__away-team__score">{awayScore}</h3>
       </div>
