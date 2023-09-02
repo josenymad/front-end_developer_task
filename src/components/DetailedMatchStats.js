5a95475c2361663624899e8d6aac872e0428d067
Jose Wills <jose.wills3@gmail.com>
2023-09-02 11:25:27 +0100

    added detailed match stats component, added styling to improve layout

---
+++ src/components/DetailedMatchStats.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "../styles/detailed-match-stats.css";
+
+const DetailedMatchStats = ({ liveData }) => {
+  // destructuring to save variables for rendering
+  const [
+    { value: homeTeamShots },
+    { value: homeTeamShotsOnTarget },
+    { value: homeTeamCorners },
+    { value: homeTeamPossession },
+  ] = liveData.lineups.home.stats;
+  const [
+    ,
+    ,
+    { value: awayTeamCorners },
+    { value: awayTeamShotsOnTarget },
+    { value: awayTeamShots },
+    { value: awayTeamPossession },
+  ] = liveData.lineups.away.stats;
+
+  return (
+    <div className="detailed-match-stats">
+      <h4 className="detailed-match-stats__full-time__header">Full Time</h4>
+      <div className="detailed-match-stats__full-time__container">
+        <div className="detailed-match-stats__full-time__home-team">
+          <p className="detailed-match-stats__full-time__home-team__shots">
+            {homeTeamShots}
+          </p>
+          <p className="detailed-match-stats__full-time__home-team__shots-on-target">
+            {homeTeamShotsOnTarget}
+          </p>
+          <p className="detailed-match-stats__full-time__home-team__corners">
+            {homeTeamCorners}
+          </p>
+          <p className="detailed-match-stats__full-time__home-team__possession">
+            {homeTeamPossession}%
+          </p>
+        </div>
+        <div className="detailed-match-stats__full-time__descriptions">
+          <p className="detailed-match-stats__full-time__descriptions__shots">
+            Shots
+          </p>
+          <p className="detailed-match-stats__full-time__descriptions__shots-on-target">
+            Shots On Target
+          </p>
+          <p className="detailed-match-stats__full-time__descriptions__corners">
+            Corners
+          </p>
+          <p className="detailed-match-stats__full-time__descriptions__possession">
+            Possession
+          </p>
+        </div>
+        <div className="detailed-match-stats__full-time__away-team">
+          <p className="detailed-match-stats__full-time__away-team__shots">
+            {awayTeamShots}
+          </p>
+          <p className="detailed-match-stats__full-time__away-team__shots-on-target">
+            {awayTeamShotsOnTarget}
+          </p>
+          <p className="detailed-match-stats__full-time__away-team__corners">
+            {awayTeamCorners}
+          </p>
+          <p className="detailed-match-stats__full-time__away-team__possession">
+            {awayTeamPossession}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailedMatchStats;
--- src/components/MatchInfo.js
+++ src/components/MatchInfo.js
@@ -1,8 +1,11 @@
 import React from "react";
 import arsenalBadge from "../images/arsenal.png";
 import fulhamBadge from "../images/fulham.png";
+import DetailedMatchStats from "./DetailedMatchStats";
+import "../styles/match-info.css";
 
 const MatchInfo = ({ matchData }) => {
+  // destructuring to save variables for rendering
   const { contestant, liveData } = matchData;
   const homeTeamName = contestant[0].name;
   const awayTeamName = contestant[1].name;
@@ -11,24 +14,28 @@ const MatchInfo = ({ matchData }) => {
 
   return (
     <div className="match-info">
-      <div className="match-info__home-team">
-        <img
-          src={arsenalBadge}
-          alt="Home Team Badge"
-          className="match-info__home-team__badge"
-        />
-        <h2 className="match-info__home-team__name">{homeTeamName}</h2>
-        <h3 className="match-info__home-team__score">{homeTeamScore}</h3>
+      <div className="match-info__results">
+        <div className="match-info__home-team">
+          <img
+            src={arsenalBadge}
+            alt="Home Team Badge"
+            className="match-info__home-team__badge"
+          />
+          <h2 className="match-info__home-team__name">{homeTeamName}</h2>
+          <h3 className="match-info__home-team__score">{homeTeamScore}</h3>
+        </div>
+        <div className="match-info__away-team">
+          <img
+            src={fulhamBadge}
+            alt="Away Team Badge"
+            className="match-info__away-team__badge"
+          />
+          <h2 className="match-info__away-team__name">{awayTeamName}</h2>
+          <h3 className="match-info__away-team__score">{awayTeamScore}</h3>
+        </div>
       </div>
-      <div className="match-info__away-team">
-        <img
-          src={fulhamBadge}
-          alt="Away Team Badge"
-          className="match-info__away-team__badge"
-        />
-        <h2 className="match-info__away-team__name">{awayTeamName}</h2>
-        <h3 className="match-info__away-team__score">{awayTeamScore}</h3>
-      </div>
+
+      <DetailedMatchStats liveData={liveData} />
     </div>
   );
 };
