eaea1d27f23420ceab54b6921bda5b99d0822196
Jose Wills <jose.wills3@gmail.com>
2023-09-02 06:08:06 +0100

    destructured nested objects from api response

--- src/requests/getMatchData.js
+++ src/requests/getMatchData.js
@@ -4,14 +4,18 @@ const getMatchData = async (setTeams, setScores) => {
   const endpoint = "https://frontend-tech-test-ashy.vercel.app/api/match";
 
   try {
-    const matchData = await axios.get(endpoint);
+    // destructuring nested objects from API response
+    const {
+      data: { match },
+    } = await axios.get(endpoint);
+
     setTeams({
-      homeTeam: matchData.data.match.contestant[0].name,
-      awayTeam: matchData.data.match.contestant[1].name,
+      homeTeam: match.contestant[0].name,
+      awayTeam: match.contestant[1].name,
     });
     setScores({
-      homeScore: matchData.data.match.liveData.matchDetails.scores.total.home,
-      awayScore: matchData.data.match.liveData.matchDetails.scores.total.away,
+      homeScore: match.liveData.matchDetails.scores.total.home,
+      awayScore: match.liveData.matchDetails.scores.total.away,
     });
   } catch (error) {
     console.log(error);
