ae6ffaab23aadb601fceaf1b5501d2f6a6decc27
Jose Wills <jose.wills3@gmail.com>
2023-09-03 23:19:34 +0100

    replaced divs with ul and p with li for semantic markup and amended styling accordingly

--- src/components/DetailedMatchStats.js
+++ src/components/DetailedMatchStats.js
@@ -22,60 +22,60 @@ const DetailedMatchStats = ({ liveData }) => {
     <div className="detailed-match-stats">
       <h4 className="detailed-match-stats__full-time__header">Full Time</h4>
       <div className="detailed-match-stats__full-time__container">
-        <div className="detailed-match-stats__full-time__home-team">
-          <p className="detailed-match-stats__full-time__home-team__shots">
+        <ul className="detailed-match-stats__full-time__home-team__list">
+          <li className="detailed-match-stats__full-time__home-team__shots">
             {homeTeamShots}
-          </p>
-          <p className="detailed-match-stats__full-time__home-team__shots-on-target">
+          </li>
+          <li className="detailed-match-stats__full-time__home-team__shots-on-target">
             {homeTeamShotsOnTarget}
-          </p>
-          <p className="detailed-match-stats__full-time__home-team__corners">
+          </li>
+          <li className="detailed-match-stats__full-time__home-team__corners">
             {homeTeamCorners}
-          </p>
-          <p className="detailed-match-stats__full-time__home-team__possession">
+          </li>
+          <li className="detailed-match-stats__full-time__home-team__possession">
             {homeTeamPossession}%
-          </p>
-        </div>
-        <div className="detailed-match-stats__full-time__descriptions">
-          <p className="detailed-match-stats__full-time__descriptions__shots">
+          </li>
+        </ul>
+        <ul className="detailed-match-stats__full-time__descriptions__list">
+          <li className="detailed-match-stats__full-time__descriptions__shots">
             Shots
-          </p>
-          <p className="detailed-match-stats__full-time__descriptions__shots-on-target">
+          </li>
+          <li className="detailed-match-stats__full-time__descriptions__shots-on-target">
             Shots On Target
-          </p>
-          <p className="detailed-match-stats__full-time__descriptions__corners">
+          </li>
+          <li className="detailed-match-stats__full-time__descriptions__corners">
             Corners
-          </p>
-          <p className="detailed-match-stats__full-time__descriptions__possession">
+          </li>
+          <li className="detailed-match-stats__full-time__descriptions__possession">
             Possession
-          </p>
-          <p className="detailed-match-stats__full-time__descriptions__yellow-cards">
+          </li>
+          <li className="detailed-match-stats__full-time__descriptions__yellow-cards">
             Yellow Cards
-          </p>
-          <p className="detailed-match-stats__full-time__descriptions__red-cards">
+          </li>
+          <li className="detailed-match-stats__full-time__descriptions__red-cards">
             Red Cards
-          </p>
-        </div>
-        <div className="detailed-match-stats__full-time__away-team">
-          <p className="detailed-match-stats__full-time__away-team__shots">
+          </li>
+        </ul>
+        <ul className="detailed-match-stats__full-time__away-team__list">
+          <li className="detailed-match-stats__full-time__away-team__shots">
             {awayTeamShots}
-          </p>
-          <p className="detailed-match-stats__full-time__away-team__shots-on-target">
+          </li>
+          <li className="detailed-match-stats__full-time__away-team__shots-on-target">
             {awayTeamShotsOnTarget}
-          </p>
-          <p className="detailed-match-stats__full-time__away-team__corners">
+          </li>
+          <li className="detailed-match-stats__full-time__away-team__corners">
             {awayTeamCorners}
-          </p>
-          <p className="detailed-match-stats__full-time__away-team__possession">
+          </li>
+          <li className="detailed-match-stats__full-time__away-team__possession">
             {awayTeamPossession}%
-          </p>
-          <p className="detailed-match-stats__full-time__away-team__yellow-cards">
+          </li>
+          <li className="detailed-match-stats__full-time__away-team__yellow-cards">
             {awayTeamYellowCards}
-          </p>
-          <p className="detailed-match-stats__full-time__away-team__red-cards">
+          </li>
+          <li className="detailed-match-stats__full-time__away-team__red-cards">
             {awayTeamRedCards}
-          </p>
-        </div>
+          </li>
+        </ul>
       </div>
     </div>
   );
